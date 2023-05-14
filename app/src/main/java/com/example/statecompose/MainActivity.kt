bfbafa12a2503b89a3da5df486d2864ae4753777
ashoknayak <ashoknayak66@gmail.com>
2023-05-15 02:29:06 +0900

    First Commit

---
+++ app/src/main/java/com/example/statecompose/MainActivity.kt
@@ -0,0 +1,131 @@
+package com.example.statecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.statecompose.ui.theme.StateComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StateComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                   // Greeting("Android")
+                    //HelloContent()
+                    //HelloScreen()
+                    HelloScreenVm()
+                }
+            }
+        }
+    }
+}
+
+//Compose State
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HelloContent( ) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        var name: String by rememberSaveable {
+            mutableStateOf("")
+        }
+        if (name.isNotEmpty()) {
+            Text(
+                text = "Hello, $name!",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+        OutlinedTextField(
+            value = name ,
+            onValueChange = {name = it},
+            label = { Text("Name")}
+        )
+    }
+}
+
+class HelloViewModel: ViewModel() {
+    private val _name: MutableLiveData<String> = MutableLiveData("")
+    val name: LiveData<String> = _name
+
+    fun onNameChange(newName: String) {
+        _name.value = newName
+    }
+}
+
+@Composable
+fun HelloScreenVm(helloViewModel: HelloViewModel = viewModel()) {
+    val name: String by helloViewModel.name.observeAsState("")
+    HelloContent1(name = name, onNameChanged = {helloViewModel.onNameChange(it)})
+}
+
+@Composable
+fun HelloScreen() {
+    var name: String by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    HelloContent1(name = name, onNameChanged = {name = it})
+}
+
+//State Hoisting
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HelloContent1(name: String, onNameChanged: (String) -> Unit) {
+    Column(modifier = Modifier.padding(16.dp)) {
+        if (name.isNotEmpty()) {
+            Text(
+                text = "Hello, $name!",
+                modifier = Modifier.padding(bottom = 8.dp),
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+        OutlinedTextField(
+            value = name ,
+            onValueChange = onNameChanged,
+            label = { Text("Name")}
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StateComposeTheme {
+        Greeting("Android")
+    }
+}
